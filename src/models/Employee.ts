6689e133d14ff769112e16c37a98a913ef53893f
Erick Malan <erickjpk1234@gmail.com>
2025-02-10 22:35:29 -0500

    Refactor la interfaz de empleados y mejora la implementación de clases para empleados asalariados y por horas

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,51 +1,57 @@
-
 // Interfaz base para empleados
 export interface Employee {
     accept(visitor: EmployeeVisitor): number;
     name: string;
     getType(): string; // Método para obtener el tipo de empleado
-  }
-  
-  // Empleado asalariado
-  export class SalariedEmployee implements Employee {
-    constructor(public name: string, public salary: number, public monthsWorked: number = 1) {}
+}
+
+// Empleado asalariado
+export class SalariedEmployee implements Employee {
+    constructor(
+        public name: string,
+        public salary: number,
+        public monthsWorked: number = 1
+    ) {}
 
     accept(visitor: EmployeeVisitor): number {
-      return visitor.visitSalariedEmployee(this);
+        return visitor.visitSalariedEmployee(this);
     }
-  
+
     getType(): string {
-      return "Asalariado";
+        return "Asalariado";
     }
-  }
-  
-  // Empleado por horas
-  export class HourlyEmployee implements Employee {
-    constructor(public name: string, public hourlyRate: number, public hoursWorked: number) {}
+}
 
-  accept(visitor: EmployeeVisitor): number {
-    return visitor.visitHourlyEmployee(this);
-  }
+// Empleado por horas
+export class HourlyEmployee implements Employee {
+    constructor(
+        public name: string,
+        public hourlyRate: number,
+        public hoursWorked: number
+    ) {}
 
-  getType(): string {
-    return "Por Horas";
-  }
-  }
-  
-  // Interfaz Visitor
-  export interface EmployeeVisitor {
+    accept(visitor: EmployeeVisitor): number {
+        return visitor.visitHourlyEmployee(this);
+    }
+
+    getType(): string {
+        return "Por Horas";
+    }
+}
+
+// Interfaz Visitor
+export interface EmployeeVisitor {
     visitSalariedEmployee(employee: SalariedEmployee): number;
     visitHourlyEmployee(employee: HourlyEmployee): number;
-  }
-  
-  // Implementación del Visitor para calcular salarios
-  export class SalaryCalculator implements EmployeeVisitor {
+}
+
+// Implementación del Visitor para calcular salarios
+export class SalaryCalculator implements EmployeeVisitor {
     visitSalariedEmployee(employee: SalariedEmployee): number {
-      return employee.salary * employee.monthsWorked;
+        return employee.salary * employee.monthsWorked;
     }
-  
+
     visitHourlyEmployee(employee: HourlyEmployee): number {
-      return employee.hourlyRate * employee.hoursWorked;
+        return employee.hourlyRate * employee.hoursWorked;
     }
-  }
-  
+}
